90c392d54bff5b8d5d8c5bee05d05fd6768e1f50
PrinceQuarshie <prince.quarshie@amalitech.org>
2022-05-03 14:40:03 +0000

    factorial of 1

--- index_test.js
+++ index_test.js
@@ -1,25 +1,35 @@
 var assert = require("assert");
 var Calculate =  require('../index.js')
 
-describe('Calculate', () => {
-  describe('.factorial', () => {
-    it('result is 120 if the input 5.', () => {
-            // setup
+describe("Calculate", () => {
+  describe(".factorial", () => {
+    it("result is 120 if the input 5.", () => {
+      // setup
       const inputNumber = 5;
       const expectedResult = 120;
-            //exercise
-      const result = Calculate.factorial(inputNumber)
-            //verify
-      assert.equal(result, expectedResult)
-
+      //exercise
+      const result = Calculate.factorial(inputNumber);
+      //verify
+      assert.equal(result, expectedResult);
     });
 
-     it('result 6 if the input is 3.', () => {
+    it("result 6 if the input is 3.", () => {
       //setup
-       const inputNumber = 3;
-       const expectedResult = 6;
-            //exercise
-       const result = Calculate.factorial(inputNumber)
-            //verify
-       assert.equal(result, expectedResult)
+      const inputNumber = 3;
+      const expectedResult = 6;
+      //exercise
+      const result = Calculate.factorial(inputNumber);
+      //verify
+      assert.equal(result, expectedResult);
     });
+    it("returns 1 when you pass in 0.", () => {
+      //setup
+      const inputNumber = 0;
+      const expectedResult = 1;
+      //exercise
+      const result = Calculate.factorial(inputNumber);
+      //verify
+      assert.equal(Calculate.factorial(0), 1);
+    });
+  });
+});
